590062ba58450f8cc66abea8b092d863c1f042f6
ValentinNM <valentinm.web@gmail.com>
2022-02-28 21:16:34 +0200

    last update before deleting the directory locally

--- src/App.js
+++ src/App.js
@@ -6,50 +6,37 @@ import "./styles.css";
 
 export default function App() {
   const [contacts, setContacts] = useState([]);
-  // const [addresses, setAddresses] = useState([]);
   const [hideForm, setHideForm] = useState(true);
   const [hideEditForm, setHideEditForm] = useState(true);
-  // const [contactEdit, setContactEdit] = useState(null)
 
   // [TODO] Write a useEffect to fetch contacts here...
-  // useEffect(() => {
-  //   Promise.all([
-  //     fetch("http://localhost:3030/contacts").then((res) => res.json()),
-  //     fetch("http://localhost:3030/addresses").then((res) => res.json())
-  //   ]).then((fetchedContacts) =>  setContacts(fetchedContacts))
-  // }, []);
 
-  useEffect(() => { 
+  useEffect(() => {
     fetch(`http://localhost:3030/contacts`)
-    .then((res) => res.json())
-    .then((fetchedContacts) =>
-      setContacts(fetchedContacts))
+      .then((res) => res.json())
+      .then((fetchedContacts) => setContacts(fetchedContacts));
   }, []);
-  // console.log("contacts inside APP: ", contacts);
 
   const [userToEdit, setUserToEdit] = useState(null);
-  // console.log("userToEdit state: ", userToEdit);
+  console.log("userToEdit state: ", userToEdit);
 
   return (
     <>
       <ContactsList
         contacts={contacts}
         // addresses={addresses}
-        hideForm={hideForm} setHideForm={setHideForm}
-        hideEditForm={hideEditForm} setHideEditForm={setHideEditForm}
+        hideForm={hideForm}
+        setHideForm={setHideForm}
+        hideEditForm={hideEditForm}
+        setHideEditForm={setHideEditForm}
         setUserToEdit={setUserToEdit}
       />
-      <main>{
-      !hideForm && <CreateContactForm
-      contacts={contacts}
-      setContacts={setContacts}
-      // userToEdit={userToEdit}
-      />},
-      {!hideEditForm &&
-      <EditContactForm userToEdit={userToEdit}/>}
+      <main>
+        {!hideForm && (
+          <CreateContactForm contacts={contacts} setContacts={setContacts} />
+        )}
+        ,{!hideEditForm && <EditContactForm userToEdit={userToEdit} />}
       </main>
     </>
   );
 }
-
-
